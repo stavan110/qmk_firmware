b6289218b6599240c5d0f7190d03ef11f3908238
Stavan Patel <stavan110@gmail.com>
2022-08-16 19:07:31 -0700

    Use qwerty instead of colemak-dhm

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/stavan110/keymap.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/stavan110/keymap.h
@@ -8,9 +8,9 @@ uint16_t last_keycode;
 bool caps_word_lock_on;
 
 enum layer_names {
-    _COLEMAK_DH,
-    _PLOVER,
+    _QWERTY,
     _SYM,
+    _IDE,
     _NAV,
     _GNAV,
 #ifdef MOUSEKEY_ENABLE
@@ -26,7 +26,7 @@ enum layer_names {
 #define SYM MO(_SYM)
 #define ADJUST MO(_ADJUST)
 #define SYM_ENT LT(_SYM, KC_ENT)
-#define PLOVER TG(_PLOVER)
+#define IDE MO(_IDE)
 
 #ifdef MOUSEKEY_ENABLE
 #    define MOUSE MO(_MOUSE)
@@ -43,20 +43,19 @@ enum layer_names {
 // Miscellaneous keyboard shortcuts in direct access
 #define UNDO LCTL(KC_Z)
 #define REDO LCTL(KC_Y)
-#define COMPOSE KC_APP
 #define TG_MIC KC_F20 // Default binding for XF86AudioMicMute
 
 // Left-hand home row mods
 #define HOME_A LGUI_T(KC_A)
-#define HOME_R LALT_T(KC_R)
-#define HOME_S LSFT_T(KC_S)
-#define HOME_T LCTL_T(KC_T)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LCTL_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
 
 // Right-hand home row mods
-#define HOME_O RGUI_T(KC_O)
-#define HOME_I LALT_T(KC_I)
-#define HOME_E RSFT_T(KC_E)
-#define HOME_N RCTL_T(KC_N)
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RCTL_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SCLN RGUI_T(KC_SCLN)
 
 // One Shot Modifiers
 #define OS_LSFT OSM(MOD_LSFT)
