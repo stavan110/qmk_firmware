81f2f76b0f6fee8b08e076d0a9d7b5b8333a57ae
Stavan Patel <stavan110@gmail.com>
2022-11-08 21:33:29 -0800

    Properly use top row

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/stavan110/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/stavan110/keymap.c
@@ -13,13 +13,13 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
-           UNDO, REDO  , KC_GRV,KC_DQUO, KC_F4 , KC_F5 ,    RFTR_RENAME,COMMENT,KC_BSLS,MS_CAPS, KC_F10, KC_F11,
-        SELWORD, KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,    KC_Y   ,KC_U,     KC_I,   KC_O,   KC_P,   KC_MINS,
+           KC_EQL, KC_1  , KC_2, KC_3, KC_4 , KC_5 ,        KC_6,   KC_7,   KC_8,   KC_9,   KC_0, KC_MINS,
+        SELWORD, KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,    KC_Y   ,KC_U,     KC_I,   KC_O,   KC_P,   KC_BACKSLASH,
          KC_ESC, HOME_A, HOME_S, HOME_D, HOME_F, KC_G  ,    KC_H   ,HOME_J, HOME_K, HOME_L, HOME_SCLN,KC_QUOT,
            REPEAT,KC_Z, KC_X  , KC_C  , KC_V  , KC_B  ,     KC_N   ,KC_M  ,KC_COMM, TD_DOT,KC_SLSH,ARROW_R,
                          KC_LEFT,KC_RIGHT,                                    KC_UP, KC_DOWN,
-                                        KC_BSPC, NAV_TAB,   SYM_ENT,KC_SPC,
-                                        IDE,     OS_LSFT,   OS_RSFT, IDE,
+                                        KC_BSPC, NAV_TAB,     SYM_ENT,KC_SPC,
+                                        IDE,     OS_LSFT,     OS_RSFT, IDE,
                                         OS_LALT ,CAPS_WORD,   OS_LCTL, OS_LALT
   ),
 
