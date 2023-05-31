225907b8530f7cc8e0987f951e4d772aa7866275
Stavan Patel <stavan110@gmail.com>
2023-05-31 01:54:57 -0700

    Custom Miryoku Keymap

---
+++ keyboards/bluebell/swoop/keymaps/stavan110_miryoku/keymap.c
@@ -0,0 +1,144 @@
+// copied from users/manna-harbour_miryoku/manna-harbour_miryoku.h
+// Copyright 2022 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#include "miryoku_babel/miryoku_layer_selection.h"
+#include "miryoku_babel/miryoku_layer_list.h"
+
+enum miryoku_layers {
+#define MIRYOKU_X(LAYER, STRING) U_##LAYER,
+MIRYOKU_LAYER_LIST
+#undef MIRYOKU_X
+};
+
+#define U_MACRO_VA_ARGS(macro, ...) macro(__VA_ARGS__)
+
+#if !defined (MIRYOKU_MAPPING)
+  #define MIRYOKU_MAPPING LAYOUT_miryoku
+#endif
+
+#define U_NP KC_NO // key is not present
+#define U_NA KC_NO // present but not available for use
+#define U_NU KC_NO // available but not used
+
+#if defined (MIRYOKU_CLIPBOARD_FUN)
+  #define U_RDO KC_AGIN
+  #define U_PST KC_PSTE
+  #define U_CPY KC_COPY
+  #define U_CUT KC_CUT
+  #define U_UND KC_UNDO
+#elif defined (MIRYOKU_CLIPBOARD_MAC)
+  #define U_RDO SCMD(KC_Z)
+  #define U_PST LCMD(KC_V)
+  #define U_CPY LCMD(KC_C)
+  #define U_CUT LCMD(KC_X)
+  #define U_UND LCMD(KC_Z)
+#elif defined (MIRYOKU_CLIPBOARD_WIN)
+  #define U_RDO C(KC_Y)
+  #define U_PST C(KC_V)
+  #define U_CPY C(KC_C)
+  #define U_CUT C(KC_X)
+  #define U_UND C(KC_Z)
+#else
+  #define U_RDO KC_AGIN
+  #define U_PST S(KC_INS)
+  #define U_CPY C(KC_INS)
+  #define U_CUT S(KC_DEL)
+  #define U_UND KC_UNDO
+#endif
+
+
+// copied from users/manna-harbour_miryoku/manna-harbour_miryoku.c
+// Copyright 2022 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#include QMK_KEYBOARD_H
+
+
+// Additional Features double tap guard
+
+enum {
+    U_TD_BOOT,
+#define MIRYOKU_X(LAYER, STRING) U_TD_U_##LAYER,
+MIRYOKU_LAYER_LIST
+#undef MIRYOKU_X
+};
+
+void u_td_fn_boot(tap_dance_state_t *state, void *user_data) {
+  if (state->count == 2) {
+    reset_keyboard();
+  }
+}
+
+#define MIRYOKU_X(LAYER, STRING) \
+void u_td_fn_U_##LAYER(tap_dance_state_t *state, void *user_data) { \
+  if (state->count == 2) { \
+    default_layer_set((layer_state_t)1 << U_##LAYER); \
+  } \
+}
+MIRYOKU_LAYER_LIST
+#undef MIRYOKU_X
+
+tap_dance_action_t tap_dance_actions[] = {
+    [U_TD_BOOT] = ACTION_TAP_DANCE_FN(u_td_fn_boot),
+#define MIRYOKU_X(LAYER, STRING) [U_TD_U_##LAYER] = ACTION_TAP_DANCE_FN(u_td_fn_U_##LAYER),
+MIRYOKU_LAYER_LIST
+#undef MIRYOKU_X
+};
+
+
+// keymap
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+#define MIRYOKU_X(LAYER, STRING) [U_##LAYER] = U_MACRO_VA_ARGS(MIRYOKU_LAYERMAPPING_##LAYER, MIRYOKU_LAYER_##LAYER),
+MIRYOKU_LAYER_LIST
+#undef MIRYOKU_X
+};
+
+
+// shift functions
+
+const key_override_t capsword_key_override = ko_make_basic(MOD_MASK_SHIFT, CW_TOGG, KC_CAPS);
+
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &capsword_key_override,
+    NULL
+};
+
+
+// thumb combos
+
+#if defined (MIRYOKU_KLUDGE_THUMBCOMBOS)
+const uint16_t PROGMEM thumbcombos_base_right[] = {LT(U_SYM, KC_ENT), LT(U_NUM, KC_BSPC), COMBO_END};
+const uint16_t PROGMEM thumbcombos_base_left[] = {LT(U_NAV, KC_SPC), LT(U_MOUSE, KC_TAB), COMBO_END};
+const uint16_t PROGMEM thumbcombos_nav[] = {KC_ENT, KC_BSPC, COMBO_END};
+const uint16_t PROGMEM thumbcombos_mouse[] = {KC_BTN2, KC_BTN1, COMBO_END};
+const uint16_t PROGMEM thumbcombos_media[] = {KC_MSTP, KC_MPLY, COMBO_END};
+const uint16_t PROGMEM thumbcombos_num[] = {KC_0, KC_MINS, COMBO_END};
+  #if defined (MIRYOKU_LAYERS_FLIP)
+const uint16_t PROGMEM thumbcombos_sym[] = {KC_UNDS, KC_LPRN, COMBO_END};
+  #else
+const uint16_t PROGMEM thumbcombos_sym[] = {KC_RPRN, KC_UNDS, COMBO_END};
+  #endif
+const uint16_t PROGMEM thumbcombos_fun[] = {KC_SPC, KC_TAB, COMBO_END};
+combo_t key_combos[COMBO_COUNT] = {
+  COMBO(thumbcombos_base_right, LT(U_FUN, KC_DEL)),
+  COMBO(thumbcombos_base_left, LT(U_MEDIA, KC_ESC)),
+  COMBO(thumbcombos_nav, KC_DEL),
+  COMBO(thumbcombos_mouse, KC_BTN3),
+  COMBO(thumbcombos_media, KC_MUTE),
+  COMBO(thumbcombos_num, KC_DOT),
+  #if defined (MIRYOKU_LAYERS_FLIP)
+  COMBO(thumbcombos_sym, KC_RPRN),
+  #else
+  COMBO(thumbcombos_sym, KC_LPRN),
+  #endif
+  COMBO(thumbcombos_fun, KC_APP)
+};
+#endif
+
+
