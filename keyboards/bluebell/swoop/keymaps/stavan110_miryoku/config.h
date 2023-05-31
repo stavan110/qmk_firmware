4fbe508820e2f27921b3963eb7500a45b10987cc
Stavan Patel <stavan110@gmail.com>
2023-05-31 02:14:31 -0700

    Customize alphas, home row mods timeout

--- keyboards/bluebell/swoop/keymaps/stavan110_miryoku/config.h
+++ keyboards/bluebell/swoop/keymaps/stavan110_miryoku/config.h
@@ -13,7 +13,7 @@
 #define TAPPING_TERM 275
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
+// #define IGNORE_MOD_TAP_INTERRUPT
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
